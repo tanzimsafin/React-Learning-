7a1907d1e195c7f4ac6aadcc1fbcc038d9c46751
tanzim_safin <tanzimhossainsafin756@gmail.com>
2024-10-18 15:33:07 +0600

    use effect use state and list and keys

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import { Component, useEffect, useState } from "react";
+
+function App() {
+  const [count, setRenderState] = useState(0);
+  useEffect(()=>{
+    console.log(`Count has change ${count}`);
+  },[count]);
+  
+  function SetState() {
+    setRenderState(count=>count+1);
+  }
+
+  return (
+    <div>
+      <button onClick={SetState}>Addtodo</button>
+      {console.log("reRender")}
+      {Array.from({ length: count }, (_, i) => (
+        <Addtodo key={i} />
+      ))}
+    </div>
+  );
+}
+
+function Addtodo() {
+  const data=[{
+    title:"Hi there!",
+    message:"Hey folk you good to go?"
+ },{
+  title:"Bye There!",
+  message:"sorry you have to go folk?"
+ }];
+  return (
+    <div>
+      <Card>
+        <Todo Component={data}/>
+        <GetDate />
+        <div style={{ display: "flex" }}>
+          <img
+            style={{ borderRadius: "50%", height: "70px", width: "70px" }}
+            src="https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"
+            alt="100xDevs Logo"
+          />
+          <h2 style={{ marginLeft: "20px", color: "#1E90FF" }}>100xDevs</h2>
+        </div>
+        <p>Hi there ! welcome to Web dev cohot-3 React Module</p>
+      </Card>
+    </div>
+  );
+}
+function Todo({Component}){
+   return <div>
+     {Component.map(elem=>{
+      return <div>
+      {elem.title}
+      {elem.message}
+     </div>
+   })}
+  </div>
+}
+function GetDate() {
+  return (
+    <div style={{ display: "flex", justifyContent: "start", padding: "10px" }}>
+      {new Date().toDateString()}
+    </div>
+  );
+}
+
+function Card({ children }) {
+  return (
+    <div
+      style={{
+        backgroundColor: "#D3D3D3",
+        padding: "20px",
+        margin: "30px auto",
+        height: "20vh",
+        borderRadius: "5px",
+        boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.1)",
+        width: "50%",
+        textAlign: "center",
+        fontFamily: "sans-serif",
+        height:"50%"
+      }}
+    >
+      {children}
+    </div>
+  );
+}
+
+export default App;
